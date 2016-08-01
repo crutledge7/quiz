9b7c49f1a0e2dc78c540750c0c3d6ccddc101f70
crutleg7 <ChrisRutledge@Chriss-MacBook-Air.local>
2016-08-01 14:49:59 -0400

    new push after video 11

--- js/controller/quiz.js
+++ js/controller/quiz.js
@@ -12,5 +12,12 @@
                 
                 vm.quizMetrics = quizMetrics;
                 vm.dataService = DataService;
+                vm.questionAnswered = questionAnswered;
+                vm.activeQuestion = 0;
+                
+                function questionAnswered(){
+                    
+                
+                }
             }
  })();
